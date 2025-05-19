a0bb9eb92ac2ee686e6678647de37766d52779b5
otegami <otegami@clear-code.com>
2025-05-19 15:54:32 +0900

    remove `(template)`

--- openarm_hardware/test/test_openarm_hardware.cpp
+++ openarm_hardware/test/test_openarm_hardware.cpp
@@ -1,6 +1,5 @@
 // Copyright (c) 2025, Reazon Holdings, Inc.
 // Copyright (c) 2025, Stogl Robotics Consulting UG (haftungsbeschränkt)
-// (template)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
