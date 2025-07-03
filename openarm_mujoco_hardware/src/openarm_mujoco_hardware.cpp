18543cf510d6a27fd92f0be64714510c990af318
Thomason Zhou <t95zhou@uwaterloo.ca>
2025-07-03 08:38:21 -0700

    Update header include

--- openarm_mujoco_hardware/src/openarm_mujoco_hardware.cpp
+++ openarm_mujoco_hardware/src/openarm_mujoco_hardware.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "openarm_mujoco_hardware/openarm_mujoco_hardware.hpp"
+#include <openarm_mujoco_hardware/openarm_mujoco_hardware.hpp>
 
 namespace openarm_mujoco_hardware {
 
