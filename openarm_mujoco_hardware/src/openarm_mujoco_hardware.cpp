bb2ee418269191c67fb52c37f4b427d4f0497a72
Thomason Zhou <t95zhou@uwaterloo.ca>
2025-07-05 15:31:44 -0700

    Remove unused header

--- openarm_mujoco_hardware/src/openarm_mujoco_hardware.cpp
+++ openarm_mujoco_hardware/src/openarm_mujoco_hardware.cpp
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <algorithm>
 #include <openarm_mujoco_hardware/openarm_mujoco_hardware.hpp>
 #include <rclcpp/logging.hpp>
 
